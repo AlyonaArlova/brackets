9c7ec62e47461548e7bd1049f38f2c832d6d366e
Alyona Arlova <ar4alyona@gmail.com>
2023-02-09 11:12:41 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const open = new Map(bracketsConfig);
+    const close = new Map(bracketsConfig.map(it => it.slice().reverse()));
+    const refs = [];
+    if (!str) {
+        return false;
+    }
+
+    const [start, ...rest] = str;
+    if (!open.has(start) || !rest.slice(-1).some(Map.prototype.has.bind(close))) {
+        return false;
+    }
+
+    for (const item of str) {
+        if (close.has(item) && refs.slice(-1).includes(close.get(item))) { 
+            refs.pop();
+        } else if (open.has(item)) {
+            refs.push(item);
+        } else {
+
+            return false;
+        }
+    }
+    return !refs.length;
 }
